51dd4f9606300390ee2599d9f9075ba9d606a80a
f-javaheri <100025483+f-javaheri@users.noreply.github.com>
2022-04-09 17:18:15 +0430

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,12 @@
 public class Main {
+  public static void main (String[] args){
+  Animal cheetah=new Cheetah("cheetah",10,"the fastest",12);
+   Animal eagle=new Eagle("eagle",10,"the fastest!!",12);    
+  Animal girafe=new Girafe("girafe",10,"the tallest",12);
+  Animal parrot=new Parrot("parrot",10,"intelligence",12);
+  cheetah.show();
+  eagle.show();
+  girafe.show();
+  parrot.show();
+}
 }
