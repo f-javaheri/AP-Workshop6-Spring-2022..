7771733a45959eca8c4ed09e5ab4997fa27acea9
f-javaheri <100025483+f-javaheri@users.noreply.github.com>
2022-04-09 17:25:06 +0430

    Update Parrot.java

--- src/Parrot.java
+++ src/Parrot.java
@@ -2,5 +2,8 @@ public class Parrot extends Bird implements Prey {
     public Parrot(String name, int age, String description, float heightOfFly) {
     super(name, age, description, heightOfFly);
 }
-
+ @Override
+    public void show() {
+        System.out.println("name : " +  getName() + ",  age : " +getAge() + ",  height of fly ,\"" + getDescription() + "\"");
+    }
 }
