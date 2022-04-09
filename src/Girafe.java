dff53c15bbac3f2329eedec614f83e7f623b7087
f-javaheri <100025483+f-javaheri@users.noreply.github.com>
2022-04-09 17:25:34 +0430

    Update Girafe.java

--- src/Girafe.java
+++ src/Girafe.java
@@ -2,4 +2,8 @@ public class Girafe extends Mammal implements Prey {
     public Girafe(String name, int age, String description, float speed) {
     super(name, age, description, speed);
 }
+     @Override
+    public void show() {
+        System.out.println("name : " +  getName() + ",  age : " +getAge() + ",  height of fly ,\"" + getDescription() + "\"");
+    }
 }
